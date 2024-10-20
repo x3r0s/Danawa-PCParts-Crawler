085278c9f379b3863d7f40c055cf3f4464f4fdae
Xeros <xeros@xbyteventures.com>
2024-10-20 16:04:49 +0900

    Fixed: Github Actions 실패에 따른 패치

--- crawler.py
+++ crawler.py
@@ -25,7 +25,9 @@ def setup_driver():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    
+    chrome_driver_path = os.environ.get("CHROMEWEBDRIVER")
+    driver = webdriver.Chrome(executable_path=chrome_driver_path, options=options)
     return driver
 
 # 스크롤 함수
